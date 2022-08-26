c71acd5c863d16d7ed45075c831d2b39d4e3ed1c
Guido <guidocalciano@gmail.com>
2022-08-26 16:43:06 -0300

    first version finished

--- script.js
+++ script.js
@@ -10,10 +10,8 @@ presentColor = bulb.style.color
 
     if (presentColor === 'rgb(255, 252, 87)') {
         bulb.style.color = "unset"
-        console.log(presentColor)
     } else {
         bulb.style.color = "rgb(255, 252, 87)"
-        console.log(presentColor)
     }
 }
 
@@ -35,10 +33,6 @@ function changeText () {
             counter++
             break;
         case 2:
-            btn_eng.innerHTML = "Top notch"
-            counter++
-            break;
-        case 3:
             btn_eng.innerHTML = "C2 English Level"
             counter++
             break;
@@ -48,9 +42,93 @@ function changeText () {
 
 // CREATIVE BUTTON
 const btn_crea = document.getElementById('creative-button');
+const btn_inno_text = document.getElementById('bulb-button-text');
+const btn_web_dev = document.getElementById('web-dev');
+const title = document.getElementById('title')
+const cat = document.getElementById('cat-loader')
+let counter_creative = 0
 
 btn_crea.addEventListener('click', changeCreativeText)
 
 function changeCreativeText () {
-    btn_crea.innerHTML = "Creative+"
+
+
+    switch (counter_creative){
+        case 0:
+            btn_crea.innerHTML = "Loading ░░░░░0%"
+            // btn_inno_text.innerText = ""
+            // btn_web_dev.innerText = ""
+            counter_creative++
+            break;
+        case 1:
+            btn_crea.innerHTML = "Loading ▓░░░░20%"
+            counter_creative++
+            break;
+        case 2:
+            btn_crea.innerHTML = "Loading ▓▓░░░40%"
+            counter_creative++
+            break;
+        case 3:
+            btn_crea.innerHTML = "Loading ▓▓▓░░60%"
+            counter_creative++
+            break;
+        case 4:
+            btn_crea.innerHTML = "Loading ▓▓▓▓░80%"
+            counter_creative++
+            break;
+        case 5:
+            btn_crea.innerHTML = "Loading ▓▓▓▓▓100%"
+            counter_creative++
+            break;
+        case 6:
+            btn_crea.innerHTML = "Done!"
+            counter_creative++
+            break;
+        case 7:
+            btn_crea.innerHTML = "Cat Loaded."
+            cat.innerHTML = `<i class="fa-solid fa-cat cat"></i>`
+            counter_creative++
+            break;
+        case 8:
+            btn_crea.innerHTML = "Creative."
+            counter_creative++
+            break;
+        case 9:
+            btn_crea.innerHTML = "Again?"
+            counter_creative++
+            break;    
+        case 10:
+            btn_crea.innerHTML = "Ok here we go."
+            counter_creative++
+            break;    
+        case 11:
+            btn_crea.innerHTML = "Cat Loaded."
+            cat.innerHTML += `<i class="fa-solid fa-cat cat"></i>`
+            counter_creative++
+            break;
+        case 12:
+            btn_crea.innerHTML = "Creative."
+            counter_creative++
+            break;
+        case 13:
+            btn_crea.innerHTML = "Really?"
+            counter_creative++
+            break;    
+        case 14:
+            btn_crea.innerHTML = "Cat Loaded."
+            cat.innerHTML += `<i class="fa-solid fa-cat cat"></i>`
+            counter_creative++
+            break;
+        case 15:
+            btn_crea.innerHTML = "One more?"
+            counter_creative++
+            break;
+        case 16:
+            btn_crea.innerHTML = "Cat Loaded."
+            cat.innerHTML += `<i class="fa-solid fa-cat cat"></i>`
+            counter_creative=16
+            break;
+            break;
+        default:
+    }
 }
